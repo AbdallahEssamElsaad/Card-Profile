c163fc49153392ed95c1e28bc7b49dc33b4ba3f7
AbdallahEssamElsaad <148257935+AbdallahEssamElsaad@users.noreply.github.com>
2023-10-17 20:06:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text", {
+    strings:["Dev : Abdallah Essam"] ,
+    typeSpeed:200,
+    backSpeed:200,
+    backDelay:1000,
+    loop:true
+});
+
+
+var typed = new Typed(".text2", {
+    strings:["Web Devoleper and Graphic Designer"] ,
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:false
+});
